cf1b85ea908bc2956d3111081f4e8a7568b9467e
Jose Ortiz Costa <jose@jortizsd.com>
2018-04-04 17:10:13 -0700

    Implemented DBConn and DBData classes. So now, you can easily insert, select, delete and update from the database. In addiiton, implemented the GatorSearch.php page. Now, an user can use the search bar to match trending topics in the database.

--- www/Database/DBConfig.php
+++ www/Database/DBConfig.php
@@ -1,8 +1,11 @@
 <?php
-    define("DB_HOST", "localhost");
-   // define("DB_SERVER", "localhost");
+
+// THIS IS A CONFIGURATION FILE. PLEASE, DON'T PUSH THIS FILE IF YOU NEED TO PUT A REAL PASSWORD IN CONSTANT DB_PASSWORD. ALWAYS KEEP A LOCAL VERSION OF THIS FILE
+
+
+define("DB_HOST", "localhost");
 define("DB_USERNAME", "root");
-define("DB_PASSWORD", "");
+define("DB_PASSWORD", "root");
 define("DATABASE", "searchResults");
 //mysqli_connect(DB_HOST , DB_SERVER , DB_USER, DB_PASSWORD, DB_DATABASE) or die("Connection error to database:  ".mysqli_error());
  // mysqli_connect("localhost", "root", "YES") or die("Connection error to database:  ".mysqli_error());
---
+++ www/Database/DBData.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: school-station
+ * Date: 4/4/18
+ * Time: 2:36 PM
+ */
+
+class DBData
+{
+
+}
