3e17b8da245a2f02af3a3e25776bf0d203a63dd6
Stuart Castaneda <Zubake@sfs-wifi-dhcp-10-143-105-90.wifi.sfsu.edu>
2018-05-03 12:55:46 -0700

    heroku DBconfig

--- www/Database/DBConfig.php
+++ www/Database/DBConfig.php
@@ -3,11 +3,11 @@
 // THIS IS A CONFIGURATION FILE. PLEASE, DON'T PUSH THIS FILE IF YOU NEED TO PUT A REAL PASSWORD IN CONSTANT DB_PASSWORD. ALWAYS KEEP A LOCAL VERSION OF THIS FILE
 
 
-define("DB_HOST", "localhost");
+define("DB_HOST", "us-cdbr-iron-east-05.cleardb.net");
 
-define("DB_USERNAME", "root");
-define("DB_PASSWORD", "");
-define("DATABASE", "searchResults");
+define("DB_USERNAME", "bf48fc0eb2a147");
+define("DB_PASSWORD", "e70aefba");
+define("DATABASE", "heroku_52f0756112bfcf9");
 
 
 //mysqli_connect(DB_HOST , DB_SERVER , DB_USER, DB_PASSWORD, DB_DATABASE) or die("Connection error to database:  ".mysqli_error());
